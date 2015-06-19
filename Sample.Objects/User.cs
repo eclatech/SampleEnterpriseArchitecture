89df9cd062e107f124a5fe63443c08502ae0086e
eclatech <eclatechsolutions@gmail.com>
2015-06-19 08:48:40 +0530

    v0

--- Sample.Objects/User.cs
+++ Sample.Objects/User.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Sample.Objects
 {
-    public class User : BaseEntity
+    public class User : BaseEntity 
     {
         public long UserId { get; set; }
         public string UserName { get; set; }
